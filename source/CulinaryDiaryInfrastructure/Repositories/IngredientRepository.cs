046806b074863192ecb37166b395465c2e41d650
MaciejGrodecki <maciej.grodecki@outlook.com>
2022-07-12 21:10:55 +0200

    Add empty IngredientRepository.

---
+++ source/CulinaryDiaryInfrastructure/Repositories/IngredientRepository.cs
@@ -0,0 +1,31 @@
+﻿using CulinaryDiaryCore.Domain;
+
+namespace CulinaryDiaryInfrastructure.Repositories;
+
+public class IngredientRepository : IIngredientRepository
+{
+    public Task<IEnumerable<Ingredient>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Ingredient> GetAsync(Guid ingredientId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(Ingredient ingredient)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Ingredient ingredient)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(Guid ingredientId)
+    {
+        throw new NotImplementedException();
+    }
+}
