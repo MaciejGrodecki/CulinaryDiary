1a09cf33a66a4be57f7590e2e88fd77dc808a2c1
MaciejGrodecki <maciej.grodecki@outlook.com>
2022-07-12 21:08:19 +0200

    Add IngredientRepository interface.

---
+++ source/CulinaryDiaryCore/Repositories/IIngredientRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace CulinaryDiaryCore.Repositories;
+
+public interface IIngredientRepository
+{
+    Task<IEnumerable<Ingredient>> GetAllAsync();
+    Task<Ingredient> GetAsync(Guid ingredientId);
+    Task AddAsync(Ingredient ingredient);
+    Task UpdateAsync(Ingredient ingredient);
+    Task DeleteAsync(Guid ingredientId);
+}
