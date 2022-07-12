e781baa647a0855c9395a6b75f98a782d4737c47
MaciejGrodecki <maciej.grodecki@outlook.com>
2022-07-12 21:04:31 +0200

    Add Ingredient class.

---
+++ source/CulinaryDiaryCore/Domain/Ingredient.cs
@@ -0,0 +1,44 @@
+﻿using CulinaryDiaryCore.Enums;
+
+namespace CulinaryDiaryCore.Domain;
+
+public class Ingredient
+{
+    public Guid IngredientId { get; set; }
+    public string Name { get; set; }
+    public double Quantity { get; set; }
+    public QuantityType QuantityType { get; set; }
+
+    protected Ingredient()
+    {
+
+    }
+
+    public Ingredient(string name, double quantity, QuantityType quantityType)
+    {
+        IngredientId = Guid.NewGuid();
+        SetName(name);
+        SetQuantity(quantity);
+        QuantityType = quantityType;
+    }
+
+    public void SetName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            throw new Exception("Name cannot be empty.");
+        }
+
+        Name = name;
+    }
+
+    public void SetQuantity(double quantity)
+    {
+        if (quantity <= 0)
+        {
+            throw new Exception("Quantity must be higher than zero.");
+        }
+
+        Quantity = quantity;
+    }
+}
