cf851daea8ede2139e01c105aadbb42671342b65
MaciejGrodecki <maciej.grodecki@outlook.com>
2022-07-13 19:42:25 +0200

    Add CulinaryDiaryContext class.

---
+++ source/CulinaryDiaryInfrastructure/Database/CulinaryDiaryContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace CulinaryDiaryInfrastructure.Database;
+public class CulinaryDiaryContext : DbContext
+{
+    private SqlServerSettings _settings;
+
+    public DbSet<Ingredient> Ingredients;
+
+    public CulinaryDiaryContext(DbContextOptions<CulinaryDiaryContext> options, IOptions<SqlServerSettings> settings)
+        : base(options)
+    {
+        _settings = settings.Value;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_settings.ConnectionString, x => x.MigrationsAssembly("CulinaryDiaryAPI"));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var ingredientBuilder = modelBuilder.Entity<Ingredient>();
+        ingredientBuilder.HasKey(i => i.IngredientId);
+    }
+}
