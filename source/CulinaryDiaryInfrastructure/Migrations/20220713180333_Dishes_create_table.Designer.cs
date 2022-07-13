6b28aeca5d5b07f4c8713b6fee022ca51fed900d
MaciejGrodecki <maciej.grodecki@outlook.com>
2022-07-13 20:03:52 +0200

    Add migration to create dishes table.

--- source/CulinaryDiaryInfrastructure/Migrations/20220713180333_Dishes_create_table.Designer.cs
+++ source/CulinaryDiaryInfrastructure/Migrations/20220713180333_Dishes_create_table.Designer.cs
@@ -4,6 +4,7 @@ using CulinaryDiaryInfrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CulinaryDiaryInfrastructure.Migrations
 {
     [DbContext(typeof(CulinaryDiaryContext))]
-    partial class CulinaryDiaryContextModelSnapshot : ModelSnapshot
+    [Migration("20220713180333_Dishes_create_table")]
+    partial class Dishes_create_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
