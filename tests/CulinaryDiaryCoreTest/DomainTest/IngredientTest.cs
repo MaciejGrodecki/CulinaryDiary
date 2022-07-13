7e6dc9fc8d0096c37394f242af51b4f0c9bcb512
MaciejGrodecki <maciej.grodecki@outlook.com>
2022-07-13 20:51:23 +0200

    Add IngredientWithNullRecipeShouldThrowException test.

--- tests/CulinaryDiaryCoreTest/DomainTest/IngredientTest.cs
+++ tests/CulinaryDiaryCoreTest/DomainTest/IngredientTest.cs
@@ -38,6 +38,19 @@ public class IngredientTest
         Assert.Equal("Quantity must be higher than zero.", exception.Message);
     }
 
+    [Theory]
+    [MemberData(nameof(IncorrectRecipeIngredient))]
+    public void IngredientWithNullRecipeShouldThrowException(
+        string name,
+        double quantity,
+        QuantityType quantityType,
+        Recipe recipe)
+    {
+        Exception exception = Assert.Throws<Exception>(() => new Ingredient(name, quantity, quantityType, recipe));
+
+        Assert.Equal("You must specify recipe, it cannot be null.", exception.Message);
+    }
+
     public static IEnumerable<object[]> CorrectIngredient =>
         new List<object[]>
         {
@@ -56,5 +69,11 @@ public class IngredientTest
         {
             new object[] { "Garlic", 0, QuantityType.Piece, new Recipe("description") },
             new object[] { "Garlic", -1, QuantityType.Piece, new Recipe("description") }
+        };
+
+    public static IEnumerable<object[]> IncorrectRecipeIngredient =>
+        new List<object[]>()
+        {
+            new object[] { "Garlic", 5, QuantityType.Piece, null }
         };
 }
