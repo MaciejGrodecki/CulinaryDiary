ba70e8c72f868328747dc1807f8a2fe3cdd77390
MaciejGrodecki <maciej.grodecki@outlook.com>
2022-07-13 20:02:02 +0200

    Add Dish domain class.

---
+++ source/CulinaryDiaryCore/Domain/Dish.cs
@@ -0,0 +1,16 @@
+﻿namespace CulinaryDiaryCore.Domain;
+public class Dish : BaseDomainClass
+{
+    public Guid DishId { get; set; }
+
+    protected Dish()
+    {
+
+    }
+
+    public Dish(string name)
+    {
+        DishId = Guid.NewGuid();
+        SetName(name);
+    }
+}
