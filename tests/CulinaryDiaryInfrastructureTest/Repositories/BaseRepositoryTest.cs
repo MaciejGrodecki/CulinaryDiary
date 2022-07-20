ee3c926784b6d90f26b68a2528ee6d32c12e1da2
MaciejGrodecki <maciej.grodecki@outlook.com>
2022-07-20 13:41:01 +0200

    Add BaseRepositoryTest class.

---
+++ tests/CulinaryDiaryInfrastructureTest/Repositories/BaseRepositoryTest.cs
@@ -0,0 +1,25 @@
+﻿using CulinaryDiaryCore.Domain;
+
+namespace CulinaryDiary.Infrastructure.Test.Repositories;
+public class BaseRepositoryTest
+{
+    protected readonly InMemoryTestContext _context;
+    public BaseRepositoryTest()
+    {
+        _context = ContextSetup.GetInMemoryTestContext();
+        PopulateIngredientsWithData();
+    }
+
+    private void PopulateIngredientsWithData()
+    {
+        var recipe = new Recipe("desc");
+
+        _context.Ingredients.Add(
+            new Ingredient("ing_1", 1.0, CulinaryDiaryCore.Enums.QuantityType.Kilogram, recipe));
+
+        _context.Ingredients.Add(
+            new Ingredient("ing_2", 1.0, CulinaryDiaryCore.Enums.QuantityType.Kilogram, recipe));
+
+        _context.SaveChanges();
+    }
+}
