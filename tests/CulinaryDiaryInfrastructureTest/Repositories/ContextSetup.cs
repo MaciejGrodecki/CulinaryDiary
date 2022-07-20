49d09716b35b3e6c04502f5872313e2aac9fc5e3
MaciejGrodecki <maciej.grodecki@outlook.com>
2022-07-20 13:40:51 +0200

    Add ContextSetup class.

---
+++ tests/CulinaryDiaryInfrastructureTest/Repositories/ContextSetup.cs
@@ -0,0 +1,14 @@
+﻿namespace CulinaryDiary.Infrastructure.Test.Repositories;
+public static class ContextSetup
+{
+    public static InMemoryTestContext GetInMemoryTestContext()
+    {
+        var options = new DbContextOptionsBuilder<CulinaryDiaryContext>()
+                .UseInMemoryDatabase(databaseName: "IngDb")
+                .Options;
+
+        IOptions<SqlServerSettings> sqlOptions = Options.Create<SqlServerSettings>(new SqlServerSettings());
+
+        return new InMemoryTestContext(options, sqlOptions);
+    }
+}
