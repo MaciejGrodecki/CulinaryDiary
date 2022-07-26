d1bd4dbb4ebc88cf4add4428d220090ebd9b82fc
MaciejGrodecki <maciej.grodecki@outlook.com>
2022-07-26 13:24:46 +0200

    Add IDishRepository interface.

---
+++ source/CulinaryDiaryCore/Repositories/IDishRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace CulinaryDiary.Core.Repositories;
+
+public interface IDishRepository
+{
+    Task<IEnumerable<Dish>> GetAllAsync();
+    Task<Dish> GetAsync(Recipe recipe);
+    Task<Dish> GetAsync(Guid dishId);
+    Task AddAsync(Dish dish);
+    Task UpdateAsync(Dish dish);
+    Task DeleteAsync(Dish dish);
+}
