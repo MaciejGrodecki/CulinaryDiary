d4eff0f592a806f7de617dc4dee65c00c418895e
MaciejGrodecki <maciej.grodecki@outlook.com>
2022-07-26 13:04:09 +0200

    Add GetAllAsyncShoudldReturnAllRecipesIngredients

--- tests/CulinaryDiaryInfrastructureTest/Repositories/IngredientRepositoryTests.cs
+++ tests/CulinaryDiaryInfrastructureTest/Repositories/IngredientRepositoryTests.cs
@@ -1,11 +1,14 @@
-﻿using CulinaryDiaryInfrastructure.Repositories;
+﻿using CulinaryDiaryCore.Domain;
+using CulinaryDiaryInfrastructure.Repositories;
 
 namespace CulinaryDiary.Infrastructure.Test.Repositories;
 public class IngredientRepositoryTests : BaseRepositoryTest
 {
+
     public IngredientRepositoryTests()
         : base()
     {
+
     }
 
     [Fact]
@@ -16,6 +19,20 @@ public class IngredientRepositoryTests : BaseRepositoryTest
         var ingredients = await repo.GetAllAsync();
 
         Assert.Equal(2, ingredients.Count());
+    }
+
+    [Fact]
+    public async void GetAllAsyncShoudldReturnAllRecipesIngredients()
+    {
+        IngredientRepository repo = new IngredientRepository(_context);
+
+        var ingredients = await repo.GetAllAsync();
+        var recipe = ingredients.Select(x => x.Recipe).FirstOrDefault();
+
+        var recipeIngredients = await repo.GetAllAsync(recipe);
+
+        Assert.Equal(2, recipeIngredients.Count());
+
     }
 
 }
